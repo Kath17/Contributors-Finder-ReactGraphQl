1768d4b572375ce30e5ae6b624256a422d484218
Katherine <kunapilco@unsa.edu.pe>
2019-11-17 22:18:34 -0500

    Add Header
    - Adding new file called Header
    - Adding Alert when input is empty

--- ReactGraphQl/App.js
+++ ReactGraphQl/App.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert, TouchableWithoutFeedback, Keyboard} from 'react-native';
+
+import Header from './Modulos/Header';
 
 export default function App() {
   const [enteredUser, setEnteredUser] = useState('');
+  const [enteredValue, setEnteredValue] = useState('');
 
   const userInputHandler = (enteredText) => {
     setEnteredUser(enteredText);
@@ -10,14 +13,22 @@ export default function App() {
 
   const confirmInputHandler = () =>{
     if(enteredUser.length === 0){
+      Alert.alert('Input vacío','Debe ingresar algún usuario',[{text:'Ok',style: 'destructive'}])
       return;
     }
     console.log(enteredUser);
+    setEnteredValue(enteredUser);
     setEnteredUser('');
   };
 
   return (
+    <TouchableWithoutFeedback
+      onPress={()=>{
+        Keyboard.dismiss();
+      }}
+    >
     <View style={styles.container}>
+      <Header title="Github Repositories"/>
       <View style = {styles.inputContainer}>
         <TextInput
           placeholder = "Search github user"
@@ -27,13 +38,14 @@ export default function App() {
         <Button color='#7d3c98' title="Search" onPress={confirmInputHandler}/>
       </View>
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop:40,
+    //marginTop:40,
     //backgroundColor: '#fff',
     //alignItems: 'center',
     //justifyContent: 'center',
